1141d6b9e568fb008f15521887d78fe0a4df1479
saeedhalabi <saeedhalabi80@gmail.com>
2024-02-28 10:59:11 +0200

    Refactor code to improve readability and maintainability
    - Extracted checking rows, columns, and diagonals into separate functions
    - Added a function to change cell colors when a player wins
    - Improved comments and formatting for better code clarity.

--- client/game.js
+++ client/game.js
@@ -14,7 +14,6 @@ function checkWinner() {
   // boolean to check if winner is found
   let winnerFound = false;
   // define a draw vaiable and set it to true for draw functionality
-
   let draw = true;
 
   // Go through each square on the board to see what symbol (X or O) is in it and store it in the board array
@@ -28,86 +27,77 @@ function checkWinner() {
   }
   // check for a draw
   if (draw) {
-    winText.innerHTML = "It's a draw!";
-    winText.style.color = "#ff6b6b";
-    winText.style.fontWeight = "bold";
+    handleDraw();
+  }
+
+  // Check if there is a winner by checking rows, columns, and diagonals.
+  let winner = checkRows() || checkColumns() || checkDiagonals();
+
+  // If a winner is found, display the winner, update the score, disable buttons, and set winnerFound to true.
+  if (winner) {
+    displayWinner(winner);
+    updateScore(winner);
     disableButtons();
-    return;
-  }
-  // rows
-  // Check if the first row has the same symbol and is not empty
-  if (board[0] == board[1] && board[1] == board[2] && board[0] != "") {
-    document.getElementById("button" + 0).style.background = "#614385";
-    document.getElementById("button" + 1).style.background = "#614385";
-    document.getElementById("button" + 2).style.background = "#614385";
-
-    displayWinner(board[0]);
     winnerFound = true;
-    updateScore(board[0]);
-  } else if (board[3] == board[4] && board[4] == board[5] && board[3] != "") {
-    document.getElementById("button" + 3).style.background = "#614385";
-    document.getElementById("button" + 4).style.background = "#614385";
-    document.getElementById("button" + 5).style.background = "#614385";
-    displayWinner(board[3]);
-    winnerFound = true;
-    updateScore(board[3]);
-  } else if (board[6] == board[7] && board[7] == board[8] && board[6] != "") {
-    document.getElementById("button" + 6).style.background = "#614385";
-    document.getElementById("button" + 7).style.background = "#614385";
-    document.getElementById("button" + 8).style.background = "#614385";
-    displayWinner(board[6]);
-    winnerFound = true;
-    updateScore(board[6]);
-  }
-
-  // columns
-  // Check if the first column has the same symbol and is not empty
-  if (board[0] == board[3] && board[3] == board[6] && board[0] != "") {
-    document.getElementById("button" + 0).style.background = "#614385";
-    document.getElementById("button" + 3).style.background = "#614385";
-    document.getElementById("button" + 6).style.background = "#614385";
-    displayWinner(board[0]);
-    winnerFound = true;
-    updateScore(board[0]);
-  } else if (board[1] == board[4] && board[4] == board[7] && board[1] != "") {
-    document.getElementById("button" + 1).style.background = "#614385";
-    document.getElementById("button" + 4).style.background = "#614385";
-    document.getElementById("button" + 7).style.background = "#614385";
-    displayWinner(board[1]);
-    winnerFound = true;
-    updateScore(board[1]);
-  } else if (board[2] == board[5] && board[5] == board[8] && board[2] != "") {
-    document.getElementById("button" + 2).style.background = "#614385";
-    document.getElementById("button" + 5).style.background = "#614385";
-    document.getElementById("button" + 8).style.background = "#614385";
-    displayWinner(board[2]);
-    winnerFound = true;
-    updateScore(board[2]);
-  }
-
-  // Diagonals
-  // check if the diagonals match
-  if (board[0] == board[4] && board[4] == board[8] && board[0] != "") {
-    document.getElementById("button" + 0).style.background = "#614385";
-    document.getElementById("button" + 4).style.background = "#614385";
-    document.getElementById("button" + 8).style.background = "#614385";
-    displayWinner(board[0]);
-    winnerFound = true;
-    updateScore(board[0]);
-  } else if (board[2] == board[4] && board[4] == board[6] && board[2] != "") {
-    document.getElementById("button" + 2).style.background = "#614385";
-    document.getElementById("button" + 4).style.background = "#614385";
-    document.getElementById("button" + 6).style.background = "#614385";
-    displayWinner(board[2]);
-    winnerFound = true;
-    updateScore(board[2]);
-  }
-
-  if (winnerFound) {
-    disableButtons();
   }
 }
 
+function handleDraw() {
+  winText.innerHTML = "It's a draw!";
+  winText.style.color = "#ff6b6b";
+  winText.style.fontWeight = "bold";
+  disableButtons();
+  return;
+}
+
+function checkRows() {
+  // rows
+  if (board[0] == board[1] && board[1] == board[2] && board[0] != "") {
+    changeCellColors([0, 1, 2]);
+    return board[0];
+  } else if (board[3] == board[4] && board[4] == board[5] && board[3] != "") {
+    changeCellColors([3, 4, 5]);
+    return board[3];
+  } else if (board[6] == board[7] && board[7] == board[8] && board[6] != "") {
+    return board[6];
+  }
+  return null;
+}
+
+function checkColumns() {
+  // columns
+  if (board[0] == board[3] && board[3] == board[6] && board[0] != "") {
+    changeCellColors([0, 3, 6]);
+    return board[0];
+  } else if (board[1] == board[4] && board[4] == board[7] && board[1] != "") {
+    changeCellColors([1, 4, 7]);
+    return board[1];
+  } else if (board[2] == board[5] && board[5] == board[8] && board[2] != "") {
+    changeCellColors([2, 5, 8]);
+    return board[2];
+  }
+  return null;
+}
+
+function checkDiagonals() {
+  // Diagonals
+  // check if the diagonals match
+  if (board[0] == board[4] && board[4] == board[8] && board[0] != "") {
+    changeCellColors([0, 4, 8]);
+    return board[0];
+  } else if (board[2] == board[4] && board[4] == board[6] && board[2] != "") {
+    changeCellColors([2, 4, 6]);
+    return board[2];
+  }
+}
+
+function changeCellColors(cells) {
+  for (let i = 0; i < cells.length; i++) {
+    document.getElementById("button" + cells[i]).style.background = "#614385";
+  }
+}
+
+// update the score based on the winner
 function updateScore(winner) {
   if (winner === "X") {
     player1Score++;
@@ -118,6 +108,7 @@ function updateScore(winner) {
   player2ScoreDisplay.querySelector("span").innerHTML = player2Score;
 }
 
+// disable all buttons
 function disableButtons() {
   // loop through all the buttons to disable them
   for (let i = 0; i < 9; i++) {
@@ -125,14 +116,16 @@ function disableButtons() {
   }
 }
 
+// display the winner
 function displayWinner(symbol) {
   winText.innerHTML = `${symbol} wins the game!`;
   winText.style.color = "#b2f2bb";
   winText.style.fontWeight = "bold";
 }
 
-// Restart functionality
+// Restart the game
 function restartBtn() {
+  // restart all buttons to their initial state
   for (let i = 0; i < 9; i++) {
     let button = document.getElementById("button" + i);
     button.innerHTML = "";
@@ -140,12 +133,14 @@ function restartBtn() {
     button.style.background = ""; // reset button color
     currentPlayer = "X"; // resets the default player
   }
+  // reset the win text and player turn display
   winText.innerHTML = "";
   playerTurnDisplay.innerHTML = "X's turn"; // resets the turn
 }
 
-// new game functionality (creates a new game)
+// start a new game
 function newGame() {
+  // Reset all buttons to their initial state
   for (let i = 0; i < 9; i++) {
     let button = document.getElementById("button" + i);
     button.innerHTML = "";
@@ -153,15 +148,16 @@ function newGame() {
     button.style.background = ""; // reset button color
     currentPlayer = "X"; // resets the default player
   }
+  // Reset the win text, player turn display, and player scores
   winText.innerHTML = "";
   playerTurnDisplay.innerHTML = "X's turn";
   player1ScoreDisplay.querySelector("span").innerHTML = 0; // sets both values to 0
   player2ScoreDisplay.querySelector("span").innerHTML = 0;
 }
 
-// Game state
-let currentPlayer = "X"; // Tracks the current player
-// Handles a cell click event
+// track the current player and handle cell clicks
+let currentPlayer = "X";
+
 function cellClick(button) {
   // Ensure the cell is empty before placing a marker
   if (button.innerHTML === "") {
